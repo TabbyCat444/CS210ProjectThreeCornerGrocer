15d447c1e73d4752e8cdc7d3dc0d544c12887e52
Tabitha <missinlucky004@aol.com>
2023-02-16 20:06:26 -0600

    Initial commit with source cpp file and TODOs

---
+++ CS210ProjectThreeCornerGrocery/Source.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>     // multi-library include
+using namespace std;
+
+// TODO: Read in file contents to vector. Don't forget to close file.
+
+// TODO: Create printMenu function
+
+// TODO: Menu option 1
+/*
+* Prompt a user to input the item, or word, they wish to look for.
+* Return a numeric value for the frequency of the specific word.
+*/
+
+// TODO: Menu option 2
+/*
+* Print the list with numbers that represent the frequency of all items purchased.
+* The screen output should include every item (represented by a word) paired with the number of
+* times that item appears in the input file, CS210_Project_Three_Input_File.txt. For example, the file might read as follows:
+* 		Potatoes 4
+* 		Pumpkins 5
+* 		Onions 3
+*/
+
+// TODO: Menu option 3
+/*
+* Print the same frequency information for all the items in the form of a histogram.
+* Then print the name, followed by asterisks or another special character to represent the numeric amount.
+* The number of asterisks should equal the frequency read from the CS210_Project_Three_Input_File.txt file.
+* For example, if the file includes 4 potatoes, 5 pumpkins, and 3 onions, then your text-based histogram may
+* appear as represented below. However, you can alter the appearance or color of the histogram in any way you choose:
+* 		Potatoes ****
+* 		Pumpkins *****
+* 		Onions ***
+*/
+
+// TODO: Menu option 4
+/*
+* Exit the program.
+*/
+
+// TODO: Data File Creation
+/*
+* Create a data file, with the naming convention frequency.dat, for backing up your accumulated data. <br>
+* The frequency.dat file should include every item (represented by a word) paired with the number of times <br>
+* that item appears in the input file.
+*/
+
+int main() {
+
+	return 0;
+}
