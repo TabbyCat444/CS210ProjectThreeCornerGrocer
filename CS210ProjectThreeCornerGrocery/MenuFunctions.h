7475a0b0c1e2169addffc9e033c10b38ceb8c05a
Tabitha <missinlucky004@aol.com>
2023-02-16 21:01:37 -0600

    Created functions file and started printMenu function

---
+++ CS210ProjectThreeCornerGrocery/MenuFunctions.h
@@ -0,0 +1,11 @@
+#ifndef CORNERGROCERY_MENUFUNCTIONS_H_
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// function prototypes
+void printMenu();
+
+#endif     //!CORNERGROCERY_MENUFUNCTIONS_H_
